5db4b59053229b64bdd53224339c007ea45797e2
JulieChoJulie <h32cho@gmail.com>
2018-07-05 23:43:00 -0400

    change to Canada

--- js/googleMap.js
+++ js/googleMap.js
@@ -81,8 +81,9 @@ function initMap() {
 
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7413549, lng: -73.9980244},
-        zoom: 13,
+        center: {lat: 61.356137, lng: -112.396204},
+
+        zoom: 4,
         styles: styles,
         mapTypeControl: false
     });
@@ -91,8 +92,11 @@ function initMap() {
     // This autocomplete is for use in the geocoder entry box.
     var zoomAutocomplete = new google.maps.places.Autocomplete(
         document.getElementById('zoom-to-area-text'));
-    // Bias the boundaries within the map for the zoom to area text.
-    zoomAutocomplete.bindTo('bounds', map);
+    // Bias the boundaries within Canada for the zoom to area text.
+    zoomAutocomplete.setComponentRestrictions(
+        {'country': ['ca']}
+    );
+
     // Create a searchbox in order to execute a places search
     var searchBox = new google.maps.places.SearchBox(
         document.getElementById('places-search'));
